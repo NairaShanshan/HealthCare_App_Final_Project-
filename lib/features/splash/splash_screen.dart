d27cb716093442164e44328155fa5fd80fc011ba
NancyEELsayed <nancyelsayed3734@gmail.com>
2025-10-17 01:03:05 +0200

    Splash Screen-without assets

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // For Timer
 
-class SplashScreen extends StatelessWidget {
+import '../onboarding/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
