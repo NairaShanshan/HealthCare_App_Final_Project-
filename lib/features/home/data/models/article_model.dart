b2760325822a2f546467745d02efde1befbb39b6
Naira Shanshan <nirashanshan@gmail.com>
2025-11-21 13:19:57 +0200

    'Finish Articles Logic'

---
+++ lib/features/home/data/models/article_model.dart
@@ -0,0 +1,15 @@
+class ArticleModel {
+  final String imagePath;
+  final String title;
+  final String date;
+  final String readTime;
+  final String content;
+
+  ArticleModel({
+    required this.imagePath,
+    required this.title,
+    required this.date,
+    required this.readTime,
+    required this.content,
+  });
+}
